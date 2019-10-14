54ab66affa0bf1a3a4a550d91e1d6566fd5c05be
Damian Schmidt <damian.schmidt97@gmail.com>
2019-10-15 00:24:45 +0200

    Add base of evaluate function

--- NEAT/src/evaluator.py
+++ NEAT/src/evaluator.py
@@ -1,10 +1,14 @@
+import random
 from abc import ABC, abstractmethod
+
+from NEAT.src import utils
 
 
 class Evaluator(ABC):
     C1 = 1.0
     C2 = 1.0
     C3 = 0.4
+    SPECIES_THRESHOLD = 10.0
 
     def __init__(self, population_size, starting_genome, node_innovation, con_innovation):
         self.population_size = population_size
@@ -26,13 +30,63 @@ class Evaluator(ABC):
         return list_of_genomes
 
     def evaluate(self):
-        pass
+        self.reset_before_generation()
+        self.place_genomes_into_species()
+        self.remove_species_without_genomes()
+        self.evaluate_genomes_and_assign_fitness()
+        self.best_into_next_generation()
+        # Breed the rest of the genomes
+
+    def best_into_next_generation(self):
+        for s in self.species:
+            reversed_fitness_population = s.fitness_population.sort(reverse=True)
+            fittest_in_species = reversed_fitness_population[0]
+            self.next_generation.append(fittest_in_species)
+
+    def evaluate_genomes_and_assign_fitness(self):
+        for genome in self.genomes:
+            s = self.genome_species[genome]
+            score = self.evaluate_genome(genome)
+            adjust_score = score / len(self.genome_species[genome].members)
+            s.total_adjusted_fitness += adjust_score
+            self.fittest_genome[genome] = score
+            if score > self.highest_score:
+                self.highest_score = score
+                self.fittest_genome = genome
+
+    def remove_species_without_genomes(self):
+        self.species = [s for s in self.species if len(s.members) != 0]
+
+    def place_genomes_into_species(self):
+        for genome in self.genomes:
+            found_species = False
+            for s in self.species:
+                # if compatibility distance is less than species threshold then genome belongs to species
+                if utils.compatibility_distance(genome, s.mascot, self.C1, self.C2, self.C3) < self.SPECIES_THRESHOLD:
+                    s.members.append(genome)
+                    self.genome_species[genome] = s
+                    found_species = True
+                    break
+
+                if not found_species:  # if there is no species applied for genome, create new species
+                    new_species = self.Species(genome)
+                    self.species.append(new_species)
+                    self.genome_species[genome] = new_species
+
+    def reset_before_generation(self):
+        for s in self.species:
+            s.reset()
+        self.genome_fitness = {}
+        self.genome_species = {}
+        self.next_generation = []
+        self.highest_score = 0
+        self.fittest_genome = None
 
     def get_random_genome_biased_adjusted_fitness(self):
         pass
 
     @abstractmethod
-    def evaluate_genome(self):
+    def evaluate_genome(self, genome):
         pass
 
     class FitnessGenome:
@@ -43,6 +97,13 @@ class Evaluator(ABC):
     class Species:
         def __init__(self, mascot):
             self.mascot = mascot
+            self.members = [self.mascot]
+            self.fitness_population = []
+            self.total_adjusted_fitness = 0.0
+
+        def reset(self):
+            new_mascot_index = random.randint(0, len(self.members) - 1)
+            self.mascot = self.members[new_mascot_index]
             self.members = [self.mascot]
             self.fitness_population = []
             self.total_adjusted_fitness = 0.0
