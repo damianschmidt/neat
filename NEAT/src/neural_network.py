71dc441a082b5b482d523239a248a7f2ee04877e
Damian Schmidt <damian.schmidt97@gmail.com>
2019-10-20 22:16:55 +0200

    Add neurons initialization in neural network

--- NEAT/src/neural_network.py
+++ NEAT/src/neural_network.py
@@ -1,3 +1,6 @@
+from NEAT.src.neuron import Neuron
+
+
 class NeuralNetwork:
     def __init__(self, genome):
         self.input = []
@@ -6,6 +9,24 @@ class NeuralNetwork:
         self.unprocessed = []
 
         # Add neurons initialization
+        # Add nodes
+        for node_id, node in genome.dict_of_nodes.items():
+            neuron = Neuron()
+            if node.node_type == 'SENSOR':
+                neuron.add_input_connections()
+                self.input.append(node_id)
+            elif node.node_type == 'OUTPUT':
+                self.output.append(node_id)
+            self.neurons[node_id] = neuron
+
+        # Add connections
+        for connection_id, connection in genome.dict_of_connections.items():
+            if not connection.expressed:
+                continue
+            in_node = self.neurons[connection.in_node]
+            in_node.add_output_connections(connection.out_node, connection.weight)
+            out_node = self.neurons[connection.out_node]
+            out_node.add_input_connections()
 
     def calculate(self):
         pass
