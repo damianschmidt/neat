3658bdcb0beb8a127dad4211c790b338e06aae97
Damian Schmidt <damian.schmidt97@gmail.com>
2019-10-22 22:11:19 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='NEAT',
+    version='1.0.0',
+    author='Damian Schmidt',
+    author_email='damian.schmidt97@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'graphics.py'
+    ],
+)
