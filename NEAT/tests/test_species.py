11312f2bb797e3aecb1f4f4cd2640b1a331dfdb9
Damian Schmidt <damian.schmidt97@gmail.com>
2019-10-24 13:40:39 +0200

    Add Species tests

--- NEAT/tests/test_species.py
+++ NEAT/tests/test_species.py
@@ -1,6 +1,20 @@
 import unittest
+from NEAT.src.genome import Genome
+from NEAT.src.evaluator import Evaluator
 
 
 class SpeciesTestCase(unittest.TestCase):
     def test_reset(self):
-        pass
+        mascot1 = Genome()
+        mascot2 = Genome()
+        fitness_pop1 = Evaluator.FitnessGenome(mascot1, 0.15)
+        fitness_pop2 = Evaluator.FitnessGenome(mascot2, 0.12)
+        species = Evaluator.Species(mascot1)
+        species.members.append(mascot2)
+        species.fitness_population = [fitness_pop1, fitness_pop2]
+        species.total_adjusted_fitness = 0.27
+        species.reset()
+
+        self.assertEqual(1, len(species.members))
+        self.assertEqual(0, len(species.fitness_population))
+        self.assertEqual(0.0, species.total_adjusted_fitness)
