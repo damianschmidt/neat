5f3cb562164ce473c01656983af4755419a4ed03
Damian Schmidt <damian.schmidt97@gmail.com>
2019-11-13 01:01:56 +0100

    Add crossover function, size checking and string representation for genome. Add fields for NodeGene and ConnectionGene. Expand BaseGene class

--- NEAT_new/src/genes.py
+++ NEAT_new/src/genes.py
@@ -2,12 +2,31 @@ class Gene:
     def __init__(self, gene_id):
         self.gene_id = gene_id
 
+    def crossover(self, gene):
+        assert self.gene_id == gene.gene_id
+        pass
+
+    def copy(self):
+        pass
+
 
 class NodeGene(Gene):
+    def __init__(self, gene_id):
+        super().__init__(gene_id)
+        self.bias = 0.0
+        self.response = 0.0
+        self.activation = 'sigmoid'
+        self.aggregation = 'sum'
+
     def distance(self, other_node):
         pass
 
 
 class ConnectionGene(Gene):
+    def __init__(self, gene_id):
+        super().__init__(gene_id)
+        self.weight = 0.0
+        self.enabled = True
+
     def distance(self, other):
         pass
--- NEAT_new/src/genome.py
+++ NEAT_new/src/genome.py
@@ -1,15 +1,46 @@
 class Genome:
     def __init__(self, genome_id):
         self.genome_id = genome_id
-        self.connections = []
-        self.nodes = []
-        self.fitness = 0.0
+        self.connections = {}
+        self.nodes = {}
+        self.fitness = None
 
     def create_new(self):
         pass
 
-    def crossover(self):
-        pass
+    def crossover(self, genome1, genome2):
+        assert isinstance(genome1.fitness, (int, float))
+        assert isinstance(genome2.fitness, (int, float))
+
+        # choose more fit parent
+        if genome1.fitness > genome2.fitness:
+            parent1, parent2 = genome1, genome2
+        else:
+            parent2, parent1 = genome1, genome2
+
+        # inherit nodes
+        parent1_nodes = parent1.nodes
+        parent2_nodes = parent2.nodes
+
+        for gene_id, node_gene1 in parent1_nodes.items():
+            node_gene2 = parent2_nodes.get(gene_id)
+            assert gene_id not in self.nodes
+            if node_gene2 is None:
+                # more gene in fittest parent - get it
+                self.nodes[gene_id] = node_gene1.copy()
+            else:
+                # combine from both
+                self.nodes[gene_id] = node_gene1.crossover(node_gene2)
+
+        # inherit connections
+        for connection_id, connection_gene1 in parent1.connections.items():
+            connection_gene2 = parent2.connections.get(connection_id)
+            if connection_gene2 is None:
+                # excess or disjoint - copy connection from parent1
+                self.connections[connection_id] = connection_gene1.copy()
+            else:
+                # combine from both
+                self.connections[connection_id] = connection_gene1.crossover(connection_gene2)
 
     def mutate(self):
         pass
@@ -18,7 +49,16 @@ class Genome:
         pass
 
     def size(self):
-        pass
+        number_of_enabled_conn = sum([1 for connection_gene in self.connections.values() if connection_gene.enabled])
+        return len(self.nodes), number_of_enabled_conn
 
     def __str__(self):
-        pass
+        string = f'ID: {self.genome_id}\nFitness: {self.fitness}\nNodes:'
+        for k, node_gene in self.nodes.items():
+            string += f'\n\t{k} {node_gene}'
+        string += '\nConnections:'
+        connections = list(self.connections.values())
+        connections.sort()
+        for conn in connections:
+            string += f'\n\t {conn}'
+        return string
