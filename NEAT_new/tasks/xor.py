6815359b33109f4499baeaa724632e955a314ef3
Damian Schmidt <damian.schmidt97@gmail.com>
2019-11-17 16:24:40 +0100

    Add base of run xor function

--- NEAT_new/tasks/xor.py
+++ NEAT_new/tasks/xor.py
@@ -1,5 +1,7 @@
 from NEAT_new.src.config import Config
+from NEAT_new.src.genetic_algorithm import GeneticAlgorithm
 from NEAT_new.src.network import Network
+from NEAT_new.src.report import Reporter
 
 xor_inputs = [(0.0, 0.0), (0.0, 1.0), (1.0, 0.0), (1.0, 1.0)]
 xor_outputs = [(0.0,), (1.0,), (1.0,), (0.0,)]
@@ -14,10 +16,14 @@ def evaluate_genome(genomes):
             genome.fitness -= (output[0] - xor_output[0]) ** 2
 
 
-def run(configuration):
-    pass
+def run():
+    config = Config()
+    population = GeneticAlgorithm(config)
+    population.reporters.add(Reporter(True))
+    winner = population.run(evaluate_genome, 300)
+
+    print(f'\nBEST GENOME:\n{winner}')
 
 
 if __name__ == '__main__':
-    config = Config()
-    run(config)
+    run()
